e377e2ae875b419e996f741d8651305d2808fce1
Junaid Sadiq <junaidci100@gmail.com>
2024-12-12 17:48:22 -0800

    Create controller/route and auth middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -71,3 +71,68 @@ export const login = async (req, res) => {
     });
   }
 };
+
+export const logout = async (_, res) => {
+  try {
+    return res.status(200).cookie(
+      "token",
+      "",
+      { maxAge: 0 },
+      json({
+        success: true,
+        message: "Logout successfully.",
+      })
+    );
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to logout.",
+    });
+  }
+};
+
+export const getUserProfile = async (req, res) => {
+  try {
+    const userId = req.id;
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Profile not found.",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to load user.",
+    });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const userId = req.id;
+    const { name } = req.body;
+    const image = req.file;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Profile not found.",
+      });
+    }
+
+    const updated = { name, profilePicture };
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to edit profile.",
+    });
+  }
+};
